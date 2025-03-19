6ca5fe7a92164051ad4f563ee38cdc7ac5ea6617
Josh Rodd <josh@rodd.us>
2025-03-19 16:52:53 -0400

    Improve first time hints

--- setup.sh
+++ setup.sh
@@ -7,19 +7,19 @@ fi
 if ! which python3; then
     brew install python3 || exit
 fi
-if [ ! -d .venv/ ]; then python3 -m venv .venv || exit; fi
-. .venv/bin/activate
-python3 -m pip install -r requirements.txt | grep -v '^Requirement already satisfied: '
-python3 -m pip install -r test_requirements.txt | grep -v '^Requirement already satisfied: '
+if ! which poetry; then
+    brew install poetry || exit
+fi
+poetry install
+poetry env activate
 echo Before running any commands, remember to:
-echo -e '\t'. .venv/bin/activate
+echo -e '\t'poetry env activate
 echo
 echo For an end to end test run:
 echo -e '\t'pytest
 echo
 echo For sample output:
 echo
-echo -e '\t'cp freecheck.toml \~/.freecheck.toml
-echo -e '\t'./freecheck.py \| ps2pdf - sample.pdf
+echo -e '\t'./freecheck.py --conf freecheck.toml \| ps2pdf - sample.pdf
 echo
 echo and then open or print the sample.pdf file.
